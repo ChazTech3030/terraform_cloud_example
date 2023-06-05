b341787421cd3d7dc687b5012ddd0fbe33df12d1
Chester Gardner <developer.chaztech@gmail.com>
2023-06-05 10:38:54 +0100

    updated provider
    now includes access keys via the Terraform Vault

--- providers.tf
+++ providers.tf
@@ -8,5 +8,11 @@ terraform {
 }
 
 provider "aws" {
+  access_key = data.vault_generic_secret.aws_access_key.data["access_key"]
+  secret_key = data.vault_generic_secret.aws_access_key.data["secret_access_key"]
+}
 
+provider "vault" {
+  address = "https://sample-cluster-public-vault-61ba8966.18057fed.z1.hashicorp.cloud:8200"
+  token = "hvs.CAESIIZ0Nab2q8pJtzgPl3cgIWeYXkHMxV_9JQ6eqs12RSFPGicKImh2cy5SY3BuTmZvSmM1alVNYVdVaUlGTFltQW4uU1BiYnQQzwE"
 }
